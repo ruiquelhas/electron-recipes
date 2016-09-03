26459101e085714cffad32d63b02e51907f24979
Rui Quelhas <rfpquelhas@gmail.com>
2016-09-03 15:12:28 +0100

    Fix functional tests

--- test/index_test.js
+++ test/index_test.js
@@ -8,19 +8,15 @@ const path = require('path')
 const pkg = require('../package.json')
 
 describe('electron-recipes', function () {
-  let app
+  let app, options
 
   before(() => {
-    app = new Application({
+    options = {
       args: [
         path.join(__dirname, '..', 'main.js')
       ],
-      path: path.join(__dirname, '..', 'node_modules', '.bin', 'electron'),
-      startTimeout: 10000,
-      waitTimeout: 10000
-    })
-
-    return db.destroy()
+      path: path.join(__dirname, '..', 'node_modules', '.bin', 'electron')
+    }
   })
 
   afterEach(() => {
@@ -36,119 +32,160 @@ describe('electron-recipes', function () {
 
   describe('basic setup', () => {
     beforeEach(() => {
+      app = new Application(options)
+
       return app.start()
     })
 
     it('opens the app main window', () => {
       return app.client
         .waitUntilWindowLoaded()
-        .getWindowCount()
-        .then(count => assert.equal(count, 1))
+        .then(() => {
+          return app.client.getWindowCount()
+        })
+        .then(count => {
+          assert.equal(count, 1)
+        })
         .getText('.title')
-        .then(text => assert.equal(text, `${pkg.name} (v${pkg.version})`))
+        .then(text => {
+          assert.equal(text, `${pkg.name} (v${pkg.version})`)
+        })
     })
   })
 
-  describe('database integration', () => {
-    let recipe
+  describe('recipe list', () => {
+    let recipes
 
     beforeEach(() => {
-      return seeder.single({ favorite: false, difficulty: 2 })
-        .then(instance => {
-          recipe = instance
+      app = new Application(options)
+
+      return seeder.random(2)
+        .then((instances) => {
+          recipes = instances
+
+          return app.start()
         })
+    })
+
+    it('displays the existing recipes in the main window', () => {
+      const expectedTitles = recipes.map(({ title }) => title).sort()
+
+      return app.client
+        .waitUntilWindowLoaded()
+        .then(() => {
+          return app.client.elements('.recipeTitle')
+        })
+        .then(({ value }) => {
+          assert.equal(value.length, 2)
+          // Get all titles (text)
+          return Promise.all(value.map(({ ELEMENT }) => {
+            return app.client.elementIdText(ELEMENT)
+          }))
+        })
+        .then(titles => {
+          const actualTitles = titles.map(({ value }) => value).sort()
+          assert.deepEqual(actualTitles, expectedTitles)
+        })
+    })
+  })
+
+  describe('difficulty mapping', () => {
+    beforeEach(() => {
+      app = new Application(options)
+
+      return seeder.single({ difficulty: 2 })
         .then(() => {
           return app.start()
         })
     })
 
-    describe('recipe list', () => {
-      it('displays the existing recipes in the main window', () => {
-        return app.client
-          .waitUntilWindowLoaded()
-          .elements('.recipe')
-          .then(elements => {
-            assert.equal(elements.value.length, 1)
-          })
-          .getText('.recipeTitle')
-          .then(text => {
-            assert.equal(text, recipe.title)
-          })
-          .getText('.recipeDescription')
-          .then(text => {
-            assert.equal(text, recipe.description)
-          })
-      })
+    it('displays the level of the existing recipes', () => {
+      return app.client
+        .waitUntilWindowLoaded()
+        .then(() => {
+          return app.client.getText('.recipeLevel')
+        })
+        .then(text => {
+          assert.equal(text, 'Easy')
+        })
+    })
+  })
 
-      it('displays the level of the existing recipes', () => {
-        return app.client
-          .waitUntilWindowLoaded()
-          .getText('.recipeLevel')
-          .then(text => {
-            assert.equal(text, 'Easy')
-          })
-      })
+  describe('set or unset a recipe as favorite', () => {
+    beforeEach(() => {
+      app = new Application(options)
+
+      return seeder.single({ favorite: false })
+        .then(() => {
+          return app.start()
+        })
     })
 
-    describe('set or unset a recipe as favorite', () => {
-      it('updates a recipe in the database when the user clicks on the "favorite" checkbox', () => {
-        return app.client
-          .waitUntilWindowLoaded()
-          .getAttribute('.favoriteToggle', 'checked')
-          .then(attr => {
-            assert.equal(attr, null)
-          })
-          .click('.favoriteToggle')
-          .waitUntil(() => {
-            return app.client
-              .getAttribute('.favoriteToggle', 'data-up-to-date')
-              .then(attr => attr === 'true')
-          })
-          .then(() => {
-            return app.restart()
-          })
-          .then(() => {
-            return app.client.waitUntilWindowLoaded()
-              .getAttribute('.favoriteToggle', 'checked')
-          })
-          .then(attr => {
-            assert.equal(attr, 'true')
-          })
-      })
+    it('updates a recipe in the database when the user clicks on the "favorite" checkbox', () => {
+      return app.client
+        .waitUntilWindowLoaded()
+        .then(() => {
+          return app.client.getAttribute('.favoriteToggle', 'checked')
+        })
+        .then(attr => {
+          assert.equal(attr, null)
+        })
+        .click('.favoriteToggle')
+        .then(() => {
+          return app.client.getAttribute('.favoriteToggle', 'checked')
+        })
+        .then(attr => {
+          assert.equal(attr, 'true')
+        })
+        .waitUntil(() => {
+          return app.client.getAttribute('.favoriteToggle', 'data-up-to-date')
+        })
+        .then(attr => {
+          return attr === 'true'
+        }, 5000)
+    })
+  })
+
+  describe('filter list of recipes by favorite flag', () => {
+    beforeEach(() => {
+      app = new Application(options)
+
+      return seeder.single({ favorite: false })
+        .then(() => {
+          return app.start()
+        })
     })
 
-    describe('filter list of recipes by favorite flag', () => {
-      it('applies all filters for the existing recipe', () => {
-        return app.client
-          .waitUntilWindowLoaded()
-          .elements('.favoriteFilterTagList > a')
-          .then(elements => {
-            const filters = elements.value
+    it('applies all filters for the existing recipe', () => {
+      let filters
 
-            assert.equal(filters.length, 3)
-
-            return app.client
-              .waitUntilWindowLoaded()
-              // SHOW_ALL
-              .elementIdClick(filters[0].ELEMENT)
-              .elements('.recipe')
-              .then(elements => {
-                assert.equal(elements.value.length, 1)
-              })
-              // SHOW_FAVORITES
-              .elementIdClick(filters[1].ELEMENT)
-              .elements('.recipe')
-              .then(elements => {
-                assert.equal(elements.value.length, 0)
-              })
-              // SHOW_REGULARS
-              .elementIdClick(filters[2].ELEMENT)
-              .elements('.recipe')
-              .then(elements => {
-                assert(elements.value.length, 1)
-              })
-          })
-      })
+      return app.client
+        .waitUntilWindowLoaded()
+        .then(() => {
+          return app.client.elements('.favoriteFilterTagList > a')
+        })
+        .then(({ value }) => {
+          assert.equal(value.length, 3)
+          filters = value
+          // SHOW_ALL
+          return app.client.elementIdClick(filters[0].ELEMENT)
+        })
+        .elements('.recipe')
+        .then(({ value }) => {
+          assert.equal(value.length, 1)
+          // SHOW_FAVORITES
+          return app.client.elementIdClick(filters[1].ELEMENT)
+        })
+        .elements('.recipe')
+        .then(({ value }) => {
+          assert.equal(value.length, 0)
+          // SHOW_REGULARS
+          return app.client.elementIdClick(filters[2].ELEMENT)
+        })
+        .elements('.recipe')
+        .then(({ value }) => {
+          assert(value.length, 1)
+        })
     })
   })
 })
